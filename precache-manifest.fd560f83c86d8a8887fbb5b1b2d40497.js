2d43b49e99dd8a87b579c83f4fea841b4146d356
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-19 07:25:07 +0000

    Deploy aleris/redux-and-typescript-enhanced-example to github.com/aleris/redux-and-typescript-enhanced-example.git:gh-pages

---
+++ precache-manifest.fd560f83c86d8a8887fbb5b1b2d40497.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abfbf3c8e3d3915a738ef4b7ded7122e",
+    "url": "/redux-and-typescript-enhanced-example/index.html"
+  },
+  {
+    "revision": "5f137aa13a44278e966e",
+    "url": "/redux-and-typescript-enhanced-example/static/css/main.0f73c45a.chunk.css"
+  },
+  {
+    "revision": "9b50c76f1411e8f048aa",
+    "url": "/redux-and-typescript-enhanced-example/static/js/2.a34f1165.chunk.js"
+  },
+  {
+    "revision": "5f137aa13a44278e966e",
+    "url": "/redux-and-typescript-enhanced-example/static/js/main.ba46a4ea.chunk.js"
+  },
+  {
+    "revision": "7a86166d4a17341d424b",
+    "url": "/redux-and-typescript-enhanced-example/static/js/runtime-main.2115324f.js"
+  },
+  {
+    "revision": "caa4eec23abd6a2c9b4c72c517d8b4bd",
+    "url": "/redux-and-typescript-enhanced-example/static/media/bot.caa4eec2.png"
+  },
+  {
+    "revision": "515ea5653a5cf3cec62f33aa45c66c27",
+    "url": "/redux-and-typescript-enhanced-example/static/media/fox.515ea565.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/redux-and-typescript-enhanced-example/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/custom-types.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '*.svg' {
-  const content: any
-  export default content
-}
-
-declare module '*.less' {
-  const styles: { [className: string]: string }
-  export default styles
-}
